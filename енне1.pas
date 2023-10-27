b3436f7ac18a027d833aa143c6a90a43fc16e189
kkasuann2000 <144228315+kkasuann2000@users.noreply.github.com>
2023-10-27 08:36:48 +0300

    Add files via upload

---
+++ енне1.pas
@@ -0,0 +1,17 @@
+﻿program zad1;
+var x,b:real;
+  begin
+    write('Введите х');
+    readln(x);
+    if x < -9 then
+      b := sqr(x)-x
+    else
+      if x <-3 then
+        b := (cos(x)/x)*(x/cos(x))
+      else 
+        if x < 4 then
+          b := sqr(x)*power(x,3)
+        else 
+          b:= power(x,1/3)/sin(x) + x;
+   writeln('Ответ = ',b)
+  end.
---
+++ енне2.pas
@@ -0,0 +1,25 @@
+﻿program zad2; 
+ var x,y,f:real;
+   begin
+    x:=-11; 
+    while x<=6 do 
+      begin
+     if x < -10 then
+      y:=cos(x) *sqr(x);
+     if (-10<=x) and (x<-4) then
+      y:=power (x, (0.1*x/31))*power (exp(x),x/59);
+     if (-4<=x) and (x<5) then 
+       y:=ln(x);
+     if (5<=x) then
+     y:= x*sqr(x)*x-cos(x);
+     if y=y then
+        f:=1
+     else 
+       f:=0;
+     if f=1 then
+     writeln('x=' ,x:0:2 ,' ', ' y ' , y:0:4)
+     else
+     writeln('x= ',x:0:2,' ','y=net korney');
+     x:=x+0.1
+     end;
+     end.
